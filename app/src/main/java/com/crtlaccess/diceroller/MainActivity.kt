025e581f687376ccb5ae65aee8804ff97cf27730
Girum.A <galemu00@gmail.com>
2020-06-21 18:32:48 -0400

    changed minSdkVersion to 19.  added srcCompat to ImageView. modified rollDice function O

--- app/src/main/java/com/crtlaccess/diceroller/MainActivity.kt
+++ app/src/main/java/com/crtlaccess/diceroller/MainActivity.kt
@@ -1,25 +1,42 @@
 package com.crtlaccess.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
+
         rollButton.setOnClickListener {
-            rollDice()
+            rollDice(diceImage)
         }
     }
 
-    private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+    private fun rollDice(diceImage: ImageView) {
+
         var randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
